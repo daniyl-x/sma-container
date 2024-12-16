469891c15543b14dff5c8a5284f03cc4e9e9c7e8
daniyl-x <daniyl-x@proton.me>
2024-12-16 12:01:33 +0200

    Add docker files for old version

---
+++ docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PORT=${PORT:-"9999"}
+TIMEOUT=${TIMEOUT:-"30"}
+ACTIONS=${ACTIONS-}
+MODE=${MODE-}
+
+_LOG="[IP AUTODISCOVERY]"
+if [ -z "$IP" ]; then
+    echo "$_LOG IP isn't set, attempting autodiscovery"
+
+    nslookup host.docker.internal > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "$_LOG host.docker.internal detected, using it's IP"
+        IP="$(nslookup -type=a host.docker.internal | awk '/Address/ {print $NF}' | awk 'END {print}')"
+    else
+        echo "$_LOG host.docker.internal isn't detected, using the default route"
+        IP="$(hostname -i | awk '{print $NF}' | sed 's/\.[0-9]*$/.1/')"
+    fi
+fi
+
+./sys_monitor_agent $IP $PORT $TIMEOUT --actions=\'$ACTIONS\' $MODE
+
